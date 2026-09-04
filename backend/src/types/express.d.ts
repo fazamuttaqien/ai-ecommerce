29bbf213bc712094877cb79b8c83a8055cff9b0e
Faza Muttaqien <fazamttqn@gmail.com>
2026-09-04 21:01:41 +0700

    fix(backend): satisfy express user type lint

--- backend/src/types/express.d.ts
+++ backend/src/types/express.d.ts
@@ -2,7 +2,9 @@ import { IUser } from '../models/user.model';
 
 declare global {
   namespace Express {
-    interface User extends IUser {}
+    interface User extends IUser {
+      comparePassword: IUser['comparePassword'];
+    }
 
     interface Request {
       guestCartId?: string | null;
