f513a0b93c3176b1e1921befabbf198e28f7d632
Faza Muttaqien <fazamttqn@gmail.com>
2026-09-05 13:19:12 +0700

    allow unauthenticated auth status

--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -5,13 +5,13 @@ import {
   logoutController,
   authStatusController,
 } from '../controllers/auth.controller';
-import { passportAuthenticateJwt } from '../config/passport.config';
+import { optionalAuthStatus } from '../config/passport.config';
 
 const authRoutes: Router = Router();
 
 authRoutes.post('/register', registerController);
 authRoutes.post('/login', loginController);
 authRoutes.post('/logout', logoutController);
-authRoutes.get('/status', passportAuthenticateJwt, authStatusController);
+authRoutes.get('/status', optionalAuthStatus, authStatusController);
 
 export default authRoutes;
